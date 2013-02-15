b3a479d9bf529761af5b01ffd3da5eb543882073
gmeluski <gmeluski@gmail.com>
2013-02-15 17:06:11 -0500

    small changes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+function hi_omaha(geography_data) {
 
+    for (var key in geography_data) {
+        if (geography_data.hasOwnProperty(key)) {
+            var latitude = geography_data[key].geocode.latitude;
+            var longitude = geography_data[key].geocode.longitude;
+            
+        }
+    }
+
+}
